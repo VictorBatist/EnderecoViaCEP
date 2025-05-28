0b36686776b6f397cbe93c8bc71c3b3645d98217
Victor <vbatista.dev@gmail.com>
2025-05-27 23:10:55 -0300

    Adicionando teste com CEP's

--- src/models/Endereco.java
+++ src/models/Endereco.java
@@ -1,4 +1,13 @@
 package models;
 
 public record Endereco(String cep, String logradouro, String complemento, String localidade, String uf) {
+
+    @Override
+    public String toString() {
+        return "cep: " + this.cep + "\n" +
+                "logradouro: " + this.logradouro + "\n" +
+                "complemento: " + this.complemento + "\n" +
+                "localidade: " + this.localidade + "\n" +
+                "uf: " + this.uf + "\n" ;
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -3,11 +3,21 @@ package view;
 import control.ConsultaCEP;
 import models.Endereco;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ConsultaCEP consultaCEP = new ConsultaCEP();
-        Endereco novoEndereço = consultaCEP.buscaEndereco("01001000");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(novoEndereço);
+        System.out.println("""
+                Informe o CEP de sua cidade: 
+                """);
+
+        String buscaCEP = scanner.nextLine();
+
+        ConsultaCEP consultaCEP = new ConsultaCEP();
+        Endereco novoEndereco = consultaCEP.buscaEndereco(buscaCEP);
+
+        System.out.println(novoEndereco);
     }
 }
